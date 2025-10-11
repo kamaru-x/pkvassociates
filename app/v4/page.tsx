e255cdba70d612c4b73c770ca4132b0899377ae2
Kamarudheen <kamaru.office@gmail.com>
2025-10-12 03:12:52 +0530

    designed v4

---
+++ app/v4/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <div className='h-[100vh] lg:h-auto bg-[#86f7f0] flex items-center justify-center'>
+                <img className='w-150' src="/image.png" alt="" />
+            </div>
+        </>
+    )
+}
+
+export default page
