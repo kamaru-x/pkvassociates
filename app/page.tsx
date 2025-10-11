03e3252e4d589598f7e3b5c8bb307204fe951093
Kamarudheen <kamaru.office@gmail.com>
2025-10-11 23:16:54 +0530

    basic design completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,75 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const page = () => {
+    return (
+        <>
+            <div className=''>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+                <div className='grid grid-cols-1 lg:grid-cols-2'>
+                    <div className='p-5 lg:p-10 bg-pink-900 rounded-br-full'>
+                        <p className='text-white font-bold text-xl lg:text-2xl'>COMPATRIOT CONSULTANTS</p>
+                        <img src="/ganesh.png" alt="" className='mt-10' />
+                    </div>
+                    <div className='flex justify-center items-center text-center'>
+                        <p className='text-pink-900 font-bold text-5xl mt-15 mb-10 leading-normal lg:mt-0 lg:mb-0 lg:text-9xl'>Together, Let&apos;s Spin the Globe!</p>
+                    </div>
+                </div>
+
+                <div className='p-5 lg:p-10'>
+                    <div className='bg-pink-900 grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-10 rounded-2xl p-5 lg:p-10'>
+                        <div className='border-b lg:border-b-0 lg:border-r border-white py-5 lg:p-5'>
+                            <p className='text-white font-bold text-xl lg:text-2xl'>ACCOUNTS</p>
+                            <p className='text-white text-lg lg:text-xl mt-5'>Our Accounts Consulting Division is the place where you can entrust your Book Keeping, Auditing, Financial Reporting Works. You do the business, our Accountants will book it for you!</p>
+                        </div>
+                        <div className='border-b lg:border-b-0 border-white py-5 lg:p-5'>
+                            <p className='text-white font-bold text-xl lg:text-2xl'>TAX</p>
+                            <p className='text-white text-lg lg:text-xl mt-5'>Our Tax Consulting Division will handle you Tax related matters, Let it be Tax Planning, Income Tax, GST, etc. Returns Filing and Compliance; Our Tax Experts will take care of it, You will just save on taxes and be Statutory Compliant!</p>
+                        </div>
+
+                        <div className='border-b lg:border-b-0 lg:border-r border-white py-5 lg:p-5'>
+                            <p className='text-white font-bold text-xl lg:text-2xl'>LEGAL</p>
+                            <p className='text-white text-lg lg:text-xl mt-5'>Our Legal Consulting Division will support you in you legally bound matters. Let it be any Agreement drafting, Execution, Litigation, or Anything; Let our Advocates Take care of it and you be worry free!</p>
+                        </div>
+                        <div className='py-5 lg:p-5'>
+                            <p className='text-white font-bold text-xl lg:text-2xl'>TOURS</p>
+                            <p className='text-white text-lg lg:text-xl mt-5'>Our Tour Management Division will gift you with Cherishable Holidays and a Divine Experience when you Visit Keralam! Visit God&apos;s Own Country, We the God&apos;s Own People shall Host You!</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='p-10'>
+                    <p className='text-pink-900 font-bold text-xl text-center lg:text-3xl'>GST or Income Tax, You Cannot Avoid Either; So Be Worry Free At Least, Entrust on Us</p>
+                    <p className='text-center mt-10 text-xl lg:text-2xl'>Tax called by different names and seen in multiple forms is obviously a global phenomenon, and Entrepreneurs like you obviously should deal with it in the most efficient manner. Managing Accounts and tax should never harm your valuable time to be spend for growing business and nuturing family.</p>
+                </div>
+
+                <div className='p-5 lg:p-10'>
+                    <div className='bg-pink-900 grid grid-cols-1 lg:grid-cols-2 gap-5 md:gap-10 rounded-2xl p-5 lg:p-10'>
+                        <div className='border-b lg:border-b-0 lg:border-r border-white py-5 lg:p-5'>
+                            <div>
+                                <p className='text-white text-lg lg:text-xl'>Managing Partner :</p>
+                                <p className='text-white font-bold text-lg lg:text-2xl'>Praveen Krishnan Venkitachalam</p>
+                            </div>
+                            <div>
+                                <p className='text-white text-lg lg:text-xl mt-5'>Contact :</p>
+                                <p className='text-white font-bold text-lg lg:text-xl'>Phone: + 91 9446 919 703 <br /> Email: pkv@pkvassociates.co.in</p>
+                            </div>
+                            <div>
+                                <p className='text-white text-lg lg:text-xl mt-5'>Address :</p>
+                                <p className='text-white font-bold text-lg lg:text-xl'>1st Cross Road, Panampilly Nagar, Cochin - 36, Kerala.</p>
+                            </div>
+                        </div>
+                        <div className='border-white py-5 lg:p-5 md:flex items-center'>
+                            <p className='text-white font-bold text-3xl lg:text-7xl'>In Trust, <br /> We Thrust!</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='p-10'>
+                    <p className='text-pink-900 font-bold text-3xl lg:text-7xl text-center'>Sass Your Stress, Before It Stuffs</p>
+                </div>
+            </div>
+        </>
+    )
 }
+
+export default page
